ac8808a2c6bf27545eb2006c5fa8f3a2cc0f9b26
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2021-08-01 16:35:06 +0200

    Create following space page

--- src/Controller/SpaceController.php
+++ src/Controller/SpaceController.php
@@ -9,10 +9,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class SpaceController extends AbstractController
 {
     /**
+     * Create & discover spaces
      * @Route("/spaces", name="app_space_index")
      */
     public function index(): Response
     {
         return $this->render('space/index.html.twig');
+    }
+
+    /**
+     * Show questions with user's following spaces
+     * @Route("/following",name="app_space_following")
+     * @return Response
+     */
+    public function following(): Response
+    {
+        return $this->render('space/following.html.twig');
     }
 }
