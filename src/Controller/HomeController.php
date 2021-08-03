46d5e46d112df33621b895fc31ec4b4219044645
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2021-08-03 22:35:05 +0200

    Create legal notice page with content

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,9 +10,28 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
+     * @return Response
      */
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="app_about")
+     * @return Response
+     */
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig');
+    }
+
+    /**
+     * @Route("/impressum", name="app_impressum")
+     * @return Response
+     */
+    public function impressum(): Response
+    {
+        return $this->render('home/impressum.html.twig');
     }
 }
