349725da74a0ce015b7713c1f83d1c31105553b4
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2021-08-08 12:57:51 +0200

    Create css classes for like and dislike button, and create event to apply class on each button on click

--- assets/javascripts/post.js
+++ assets/javascripts/post.js
@@ -5,6 +5,8 @@ post = {
         const posts = document.querySelectorAll('.postBody');
         const commentButtons = document.querySelectorAll('.commentButton');
         const commentSection = document.querySelectorAll('.numberOfComments');
+        const likeButtons = document.querySelectorAll('.likeButton');
+        const dislikeButtons = document.querySelectorAll('.dislikeButton');
 
         for (let index = 0; index < posts.length; index++) {
             posts[index].addEventListener('click', post.handlePostDisplay);
@@ -17,12 +19,21 @@ post = {
         for (let index = 0; index < commentSection.length; index++) {
             commentSection[index].addEventListener('click', post.handleCommentDisplay);
         }
+
+        for (let index = 0; index < likeButtons.length; index++) {
+            likeButtons[index].addEventListener('click', post.handleLikeButton);
+        }
+
+        for (let index = 0; index < dislikeButtons.length; index++) {
+            dislikeButtons[index].addEventListener('click', post.handleDislikeButton);
+        }
+
     },
     handlePostDisplay: function (e) {
         e.preventDefault();
         const postToDisplay = e.target.closest('.postBody');
         const moreLink = postToDisplay.querySelector('.moreLink');
-       
+
         moreLink.style.display = 'none';
         postToDisplay.querySelector('p').removeAttribute('style');
 
@@ -40,6 +51,29 @@ post = {
             comments.style.display = 'block';
             commentList.style.display = 'none';
         };
+
+    },
+    handleLikeButton: function (e) {
+        e.preventDefault();
+        const dislikeButton = e.currentTarget.closest('.postFooter').querySelector('.dislikeButton');
+        const likeButton = e.currentTarget;
+        if (likeButton.classList.contains('liked')) {
+            likeButton.classList.remove('liked');
+        } else {
+            likeButton.classList.add('liked');
+        }
+        dislikeButton.classList.remove('disliked');
+    },
+    handleDislikeButton: function (e) {
+        e.preventDefault();
+        const likeButton = e.currentTarget.closest('.postFooter').querySelector('.likeButton');
+        const dislikeButton = e.currentTarget;
+        if (dislikeButton.classList.contains('disliked')) {
+            dislikeButton.classList.remove('disliked');
+        } else {
+            dislikeButton.classList.add('disliked');
+        }
+        likeButton.classList.remove('liked');
 
     }
 }
