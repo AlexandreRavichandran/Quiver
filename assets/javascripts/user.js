aecbf1b6801682771abd6dad59c21319cd474381
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2021-08-17 23:14:36 +0200

    Add ajax request for subscription in all pages (Not factorized)

--- assets/javascripts/user.js
+++ assets/javascripts/user.js
@@ -4,30 +4,57 @@ const user = {
         const subscribeButtons = document.querySelectorAll('.subscribeButton');
 
         for (let index = 0; index < subscribeButtons.length; index++) {
-            subscribeButtons[index].addEventListener('click', user.handleSubscription);
+            subscribeButtons[index].addEventListener('click', user.handleSubscriptionButton);
 
+        }
+
+        const subscribeLinks = document.querySelectorAll('.subscribeLink');
+
+        for (let index = 0; index < subscribeLinks.length; index++) {
+            subscribeLinks[index].addEventListener('click', user.handleSubscriptionLinks);
         }
 
     },
 
-    handleSubscription: function (e) {
+    handleSubscriptionLinks: function (e) {
         e.preventDefault();
-        const idOfUserToHandle = e.currentTarget.dataset.id;
+        const currentLink = e.currentTarget;
 
-        if (e.currentTarget.classList.contains('subscribed')) {
+        if (currentLink.classList.contains('link-subscribed')) {
 
-            user.AJAXSubscriptionHandler(idOfUserToHandle, "remove");
-            e.currentTarget.classList.add('notSubscribed');
-            e.currentTarget.classList.remove('subscribed');
-            e.currentTarget.innerHTML = "<i class='bi bi-person-plus mr-3'></i>Suivre";
+            user.AJAXSubscriptionHandler(currentLink.dataset.id, "remove");
+            currentLink.classList.add('link-unsubscribed');
+            currentLink.classList.remove('link-subscribed');
+            currentLink.textContent = "Suivre";
 
         } else {
-            user.AJAXSubscriptionHandler(idOfUserToHandle, "add");
-            e.currentTarget.classList.remove('notSubscribed');
-            e.currentTarget.classList.add('subscribed');
-            e.currentTarget.innerHTML = "<i class='bi bi-person-plus-fill mr-3'></i>Suivi";
+            user.AJAXSubscriptionHandler(currentLink.dataset.id, "add");
+            currentLink.classList.remove('link-unsubscribed');
+            currentLink.classList.add('link-subscribed');
+            currentLink.textContent = "Suivi";
+        }
+
+    },
+
+    handleSubscriptionButton: function (e) {
+        e.preventDefault();
+        const idOfUserToHandle = e.currentTarget;
+
+        if (idOfUserToHandle.classList.contains('subscribed')) {
+
+            user.AJAXSubscriptionHandler(idOfUserToHandle.dataset.id, "remove");
+            idOfUserToHandle.classList.add('notSubscribed');
+            idOfUserToHandle.classList.remove('subscribed');
+            idOfUserToHandle.innerHTML = "<i class='bi bi-person-plus mr-3'></i>Suivre";
+
+        } else {
+            user.AJAXSubscriptionHandler(idOfUserToHandle.dataset.id, "add");
+            idOfUserToHandle.classList.remove('notSubscribed');
+            idOfUserToHandle.classList.add('subscribed');
+            idOfUserToHandle.innerHTML = "<i class='bi bi-person-plus-fill mr-3'></i>Suivi";
         }
     },
+
     AJAXSubscriptionHandler: function (id, action) {
         const xhttp = new XMLHttpRequest();
 
